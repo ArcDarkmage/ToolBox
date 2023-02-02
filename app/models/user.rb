7f7bcd8fed9acc3d2e20df11674a6c1eb2e39978
Gabriel Martins <gabriel.st.martins@gmail.com>
2023-02-02 19:43:32 -0300

    Dependencies are for only 1 item at a time

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :tools, :rents
+  has_many :tools
+  has_many :rents
   validates :first_name, :last_name, :apartment, :email, presence: true
 
   devise :database_authenticatable, :registerable,
