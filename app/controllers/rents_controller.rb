b42cd968851aac094c5d6f8edaef00f2bfa6e124
Nilce Moscone <nilcemayumi@gmail.com>
2023-02-03 15:34:38 -0300

    new rent crated. Routes for all tools and rents

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -1,2 +1,26 @@
 class RentsController < ApplicationController
+  def new
+    @rent = Rent.new
+    @tool = Tool.find(params[:tool_id])
+    authorize @rent
+  end
+
+  def create
+    @rent = Rent.new(rent_params)
+    @tool = Tool.find(params[:tool_id])
+    @rent.user = current_user
+    @rent.tool = @tool
+    authorize @rent
+    if @rent.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def rent_params
+    params.require(:rent).permit(:date_start, :date_end)
+  end
 end
--- app/models/rent.rb
+++ app/models/rent.rb
@@ -2,5 +2,24 @@ class Rent < ApplicationRecord
   belongs_to :tool
   belongs_to :user
   validates :date_start, :date_end, presence: true
+  validate :date_start_and_date_end
+  validate :available
 
+  def date_start_and_date_end
+    if date_start < Date.today
+      errors.add(:date_start, "Data de início inválida")
+    elsif date_start > date_end
+      errors.add(:date_end, "Data final deve ser depois da data de início")
+    end
+  end
+
+  def available
+    tool = Tool.find(tool_id)
+    rents = tool.rents
+    date_ranges = rents.map { |rent| rent.date_start..rent.date_end }
+    date_ranges.each do |range|
+      errors.add(:date_start, "Data indisponível") if range.include?(date_start)
+      errors.add(:date_end, "Data indisponível") if range.include?(date_end)
+    end
+  end
 end
