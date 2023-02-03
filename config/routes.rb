190201361f4c3decb8b495f57a81a0a46ed27ff7
Nilce Moscone <nilcemayumi@gmail.com>
2023-02-03 19:35:13 -0300

    rota para user#show adicionada, criado o user_policy, criada a view e o controller do user

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,33 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  resources :users, only: [:show]
+
 end
