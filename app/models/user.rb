ac4901195ce2b966e683a28ac4fb2310a6ca2320
Nilce Moscone <nilcemayumi@gmail.com>
2023-02-10 01:21:46 -0300

    Valor dos alugueis arrumado

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :tools
-  has_many :rents
+  has_many :tools, dependent: :destroy
+  has_many :rents, dependent: :destroy
   has_one_attached :photo
   validates :first_name, :last_name, :apartment, :email, presence: true
 
